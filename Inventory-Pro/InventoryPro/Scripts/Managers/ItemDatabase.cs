63d018777f36f9f040827d78f69d92dcd10f6b4d
Richard Osborn <richardosborn256@gmail.com>
2023-03-26 11:21:45 -0700

    Remove deprecated objects

--- Inventory-Pro/InventoryPro/Scripts/Managers/ItemDatabase.cs
+++ Inventory-Pro/InventoryPro/Scripts/Managers/ItemDatabase.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using Devdog.InventoryPro;
-using UnityEngine;
+﻿using UnityEngine;
 using Devdog.General.ThirdParty.UniLinq;
-using UnityEngine.Serialization;
 
 namespace Devdog.InventoryPro
 {
-
     [System.Serializable]
     [CreateAssetMenu(fileName = "ItemDatabase.asset", menuName = InventoryPro.CreateAssetMenuPath + "Item Database")]
     public partial class ItemDatabase : ScriptableObject
@@ -26,34 +22,6 @@ namespace Devdog.InventoryPro
         [Header("Currencies")]
         public CurrencyDefinition[] currencies = new CurrencyDefinition[0];
 
-
-#pragma warning disable 618
-
-        [Header(" -- Deprecated")]
-
-        [Obsolete("Replaced by rarities")]
-        public InventoryItemRarityDeprecated[] itemRarities = new InventoryItemRarityDeprecated[0];
-        [Obsolete("Replaced by categories")]
-        public InventoryItemCategoryDeprecated[] itemCategories = new InventoryItemCategoryDeprecated[0];
-        [Obsolete("Replaced by properties"), FormerlySerializedAs("properties")]
-        public InventoryItemPropertyDeprecated[] itemProperties = new InventoryItemPropertyDeprecated[0];
-        
-        [Obsolete("Replaced by equipmentTypes")]
-        public InventoryEquipTypeDeprecated[] equipTypes = new InventoryEquipTypeDeprecated[0];
-
-        [Obsolete("Replaced by craftCategories")]
-        [FormerlySerializedAs("craftingCategories")]
-        public InventoryCraftingCategoryDeprecated[] inventoryCraftingCategories = new InventoryCraftingCategoryDeprecated[0];
-
-        [Obsolete("Replaced by currencies")]
-        [FormerlySerializedAs("currencies")]
-        public InventoryCurrencyDeprecated[] inventoryCurrencies = new InventoryCurrencyDeprecated[0];
-
-
-
-#pragma warning restore 618
-
-
         // Convenience methods:
         public string[] pluralCurrenciesStrings
         {
@@ -62,6 +30,7 @@ namespace Devdog.InventoryPro
                 return currencies.Select(o => o.pluralName).ToArray();
             }
         }
+
         public string[] singleCurrenciesStrings
         {
             get
@@ -85,6 +54,7 @@ namespace Devdog.InventoryPro
                 return statDefinitions.Select(o => o.statName).ToArray();
             }
         }
+
         public string[] rarityStrings
         {
             get
@@ -100,6 +70,7 @@ namespace Devdog.InventoryPro
                 return rarities.Select(o => o.color).ToArray();
             }
         }
+
         public string[] itemCategoriesStrings
         {
             get
